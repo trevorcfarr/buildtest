544c8db29ad4fbf3a45f5a66d0f812aeab6b47db
Trevor Farr <trevorfarr@MacBook-Pro-4.local>
2023-04-18 08:19:25 -0700

    adjustments to todo tasks

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# TODO General logging, Django Front End, Testing/assertions.
-# TODO remove/rework sleep
-# TODO integrate with Slack Webhook to message build passed
-# TODO reach out to DevOps and look over scripts to push builds with AWS
-# TODO possible drop down front end of AWS environments
-# TODO create decorator that says "beginning of test, initialize driver, run test, destroy driver once tests completed" this way each test is fresh
 
 # logging.basicConfig()
 # logging.root.setLevel(logging.INFO)
